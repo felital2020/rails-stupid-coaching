b6b769899c5cd0e8c12ac4e69157732e3800e0ed
Felita Liem <felital2020@gmail.com>
2022-08-16 13:54:54 +0800

    commit 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "ask", to: "questions#ask"
+
+
   # Defines the root path route ("/")
+
+  get 'answer', to: 'questions#answer'
+
+
   # root "articles#index"
+  root to: "questions#ask"
 end
